a84611919b08017e56d7d82c6f8c143fcbc48495
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-04-15 10:07:16 +0530

    Cleaning files

--- db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/taxRecord')
-  .then(() => {
-    console.log('connected');
-  })
-  .catch((error) => {
-    console.error('Error connecting to dadabase', error);
-  })
